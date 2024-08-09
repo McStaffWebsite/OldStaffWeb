9bd94f839c2a2f6b45bf71cc3ff95489f86361a2
McStaffWebsite <mcstaffwebsite@gmail.com>
2024-08-09 15:25:25 +0200

    Theme switching

---
+++ javascript/theme-switcher.js
@@ -0,0 +1,24 @@
+const switchTheme = () => {
+    const rootElem = document.documentElement;
+    let dataTheme = rootElem.getAttribute('data-theme'),
+        newTheme;
+    
+    // Determine the new theme
+    newTheme = (dataTheme === 'light') ? 'dark' : 'light';
+    
+    // Set the new theme
+    rootElem.setAttribute('data-theme', newTheme);
+    
+    // Save the new theme in localStorage
+    localStorage.setItem('theme', newTheme);
+};
+
+// Check for saved theme in localStorage and apply it
+const currentTheme = localStorage.getItem('theme') || 'light';
+document.documentElement.setAttribute('data-theme', currentTheme);
+
+// Attach event listener to the theme switcher button
+const el = document.getElementById('theme-switcher');
+if (el) {
+  el.addEventListener('click', switchTheme);
+}
